8263c26d6d9a17d2b37ab6f2a55f43fd042ae738
ThomasBelk <thomasjb03@gmail.com>
2026-01-28 09:05:21 -0500

    Application remembers window size, window position, and potentially screen. Screen is untested rn.

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 import sys
+
+from PySide6.QtGui import QCloseEvent
 from PySide6.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout
+from PySide6.QtCore import QSettings
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+        self.settings = QSettings("ThetaBork", "FacialExpressionsCompanion")
         self.containerWidget = QWidget()
         self.initUI()
+        self.restoreWindowState()
+
 
     def initUI(self):
         self.setWindowTitle("Facial Expressions Companion")
@@ -16,7 +22,34 @@ class MainWindow(QMainWindow):
         self.setCentralWidget(self.containerWidget)
         self.containerWidget.setLayout(vbox)
 
+    def restoreWindowState(self):
+        app = QApplication.instance()
 
+        if self.settings.contains("Screen") and self.settings.value("Screen"):
+            screen = self.settings.value("Screen").value()
+        else:
+            screen = app.primaryScreen()
+        screenGeo = screen.availableGeometry()
+
+        if self.settings.contains("WindowSize") and self.settings.value("WindowSize"):
+            self.resize(self.settings.value("WindowSize"))
+        else:
+            width = screenGeo.width()
+            height = screenGeo.height()
+            self.resize(int(width * 0.5), int(height * 0.6))
+
+        # window placement
+        if self.settings.contains("WindowPosition") and self.settings.value("WindowPosition"):
+            self.move(self.settings.value("WindowPosition"))
+        else:
+            self.move(screenGeo.center() - self.rect().center())
+
+    def closeEvent(self, event: QCloseEvent):
+        self.settings.setValue("WindowSize", self.size())
+        self.settings.setValue("Screen", self.screen())
+        self.settings.setValue("WindowPosition", self.pos())
+
+        event.accept()
 
 
 def main():
@@ -24,13 +57,8 @@ def main():
     app.setStyle("Fusion")
     window = MainWindow()
 
+
     # make window open in the middle of the screen at a consistent size
-    screen = app.primaryScreen()
-    screenGeo = screen.availableGeometry()
-    width = screenGeo.width()
-    height = screenGeo.height()
-    window.resize(int(width * 0.5), int(height * 0.6)) # TODO: Should probably be able to change this in settings
-    window.move(screenGeo.center() - window.rect().center())
 
     window.show()
     sys.exit(app.exec())
